2ea02ec171162304fdadfaeb72aca4aa4be9164d
ppguz <ppguzmanmayen@gmail.com>
2025-04-02 02:29:18 -0600

    Feat: added mutex to handle thread safety

--- src/server/DataSource.cpp
+++ src/server/DataSource.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <map>
 #include <unordered_set>
+#include <pthread.h>
 
 using namespace std;
 
@@ -63,92 +64,123 @@ class DataSource {
         unordered_map<lws*, User> users; // Nos ayuda a asociar una sesión con un usuario
         vector<ChatMessage> generalChat; //Vector que contiene los mensajes del chat general
         map<string, vector<ChatMessage>> privateChats;
+        pthread_mutex_t data_mutex = PTHREAD_MUTEX_INITIALIZER;
 
     public:
         vector<string> getUsernames() {
             vector<string> usernames;
+            pthread_mutex_lock(&data_mutex);
             for (const auto& pair : users) {
                 usernames.push_back(pair.second.username);
             }
+            pthread_mutex_unlock(&data_mutex);
             return usernames;
         }
 
         bool insert_user(lws* wsi, const string& username, const string& ip_addr, int status) {
             // Verificar si el username existe o es válido
+            pthread_mutex_lock(&data_mutex); //Crear el mutex
             if (username == "~" || username == "" || username.length() > 10) {
+                pthread_mutex_unlock(&data_mutex); //Desbloquear
                 return false;
             }
             for (const auto& pair : users) {
                 if (pair.second.username == username) {
+                    pthread_mutex_unlock(&data_mutex); 
                     return false; 
                 }
             }
             // Si no existe o no es un nombre inválido, insertar 
             users[wsi] = {username, ip_addr, status};
             return true;
+            pthread_mutex_unlock(&data_mutex); //Desbloquear
         }
         //Método para hallar los usuarios que están conectados
         std::vector<User> getConnectedUsers() {
+            pthread_mutex_lock(&data_mutex);
             std::vector<User> connected;
             for (const auto& [username, user] : users) {
                 if (user.status != DISCONNECTED) {
                     connected.push_back(user);
                 }
             }
+            pthread_mutex_unlock(&data_mutex);
             return connected;
         }
         //Función para hallar un usuario
         User* get_user(const std::string& username) {
-            //Iterar hasta encontrar el usuario que buscamos
+            pthread_mutex_lock(&data_mutex);
+            User* result = nullptr;
+            
             for (auto& pair : users) {
                 if (pair.second.username == username) {
-                    return &(pair.second);
+                    result = &(pair.second);
+                    break;
                 }
             }
-            return nullptr;
+            
+            pthread_mutex_unlock(&data_mutex);
+            return result;
         }
 
         void changeStatus(struct lws *wsi, int newStatus) { //Función para cambiar el status de un usuario
+            pthread_mutex_lock(&data_mutex);
             users[wsi].status = newStatus;
+            pthread_mutex_unlock(&data_mutex);
         }
 
         void insertMessage(struct lws *wsi, string reciever, string messageContent){
+            pthread_mutex_lock(&data_mutex);
             string senderName = users[wsi].username;
             if (reciever == "~"){
                 generalChat.push_back({senderName, reciever, messageContent}); //Insertar el nuevo mensaje
+                pthread_mutex_unlock(&data_mutex);
             }
             else {
                 string chatKey = (senderName < reciever) ? senderName + ":" + reciever : reciever + ":" + senderName; //Generar llava única para el chat privado
                 privateChats[chatKey].push_back({senderName, reciever, messageContent}); //Insertar mensaje en la conversación privada
+                pthread_mutex_unlock(&data_mutex);
             }
         }
         lws* get_wsi_by_username(const std::string& username) { //Busca el wsi de un user
+            pthread_mutex_lock(&data_mutex);
             for (const auto& pair : users) {
                 if (pair.second.username == username) {
+                    pthread_mutex_unlock(&data_mutex);
                     return pair.first; 
                 }
             }
+            pthread_mutex_unlock(&data_mutex);
             return nullptr;
         }
         User* get_user_by_wsi(struct lws *wsi) {
-            //Iterar hasta encontrar el usuario que buscamos
+            pthread_mutex_lock(&data_mutex);
+            User* result = nullptr;
+            
             for (auto& pair : users) {
                 if (pair.first == wsi) {
-                    return &(pair.second);
+                    result = &(pair.second);
+                    break;
                 }
             }
-            return nullptr;
+            
+            pthread_mutex_unlock(&data_mutex);
+            return result;
         } 
 
         vector<ChatMessage> getChatHistory(string name){
+            pthread_mutex_lock(&data_mutex);
             if(name == "~"){
+                pthread_mutex_unlock(&data_mutex);
                 return generalChat;
             }
             else {
                 if (privateChats.find(name) != privateChats.end()) {
+                    pthread_mutex_unlock(&data_mutex);
                     return privateChats[name];
                 } else {
                     // Si no existe, devolver un vector vacío
+                    pthread_mutex_unlock(&data_mutex);
                     return vector<ChatMessage>();
                 }
             }
